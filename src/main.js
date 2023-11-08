d0c797b8788afaa3cf318b5c58a0c744cff5e0bf
Samuel <Samuel>
2023-11-08 14:19:40 +0100

    Probleme en couleur

--- src/main.js
+++ src/main.js
@@ -990,10 +990,11 @@ class Ligne extends HTMLElement {
 
 
 //Main Programme principal de test
-document.getElementById('espace1') 
+let a = new Probleme;
+document.getElementById('espace1').appendChild(a);
+a.afficher();
 
-let a = new Probleme
-
-document.getElementById('espace1').appendChild(a) 
-
-a.afficher() 
+/*
+let b = new Procedure;
+document.getElementById('espace1').appendChild(b); 
+b.afficher(); */
