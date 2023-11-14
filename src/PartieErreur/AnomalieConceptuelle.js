63acf9aca99f573cec1258891902db7db4d97993
Samuel <samuelcurran64100@gmail.com>
2023-11-14 14:37:31 +0100

    Deplacement des fichiers en deux partie PartiErreur PartieEditeur creation de nouvelle classe dans la partieErreur

---
+++ src/PartieErreur/AnomalieConceptuelle.js
@@ -0,0 +1,22 @@
+class Condition 
+{
+    // ATTRIBUTS
+    _elementEmetteur; // Element Graphique
+
+    // CONSTRUCTEUR
+    constructor() {
+    }
+
+    // ENCAPSULATION
+    set _elementEmetteur(value)
+    {
+
+    }
+
+    get _elementEmetteur()
+    {
+        return this._elementEmetteur;
+    }
+    // METHODES
+    toString();
+} 
