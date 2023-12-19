8a6488a83afffe1b64c9fe3a1b3cf21460584608
Samuel <Samuel>
2023-12-19 15:54:11 +0100

    Avancement sur l'erreur ErreurDonneeInutilisee

--- src/PartieEditeur/ElementParent.js
+++ src/PartieEditeur/ElementParent.js
@@ -1,6 +1,6 @@
 class ElementParent {
     // ATTRIBUTS
-    _listeElementsEnfants; // Liste de dictionnaires ElementGraphique, Ligne
+    _listeElementsEnfants; // Liste contient les ElementGraphiques et les Lignes
     _proprietaire; // ElementGraphique
 
     // CONSTRUCTEUR
--- src/PartieEditeur/Probleme.js
+++ src/PartieEditeur/Probleme.js
@@ -175,10 +175,15 @@ class Probleme extends ElementGraphique {
         {
             listeAnomalies.push(new AvertissementTropDeSousElements(this, this.getEnfants()));
         }
+        // 18
+        if(AvertissementDonneDynamiquementTypee.detecterAnomalie(this))
+        {
+            listeAnomalies.push(new AvertissementDonneDynamiquementTypee(this));
+        }
         return listeAnomalies;
     }
     //Retourne les Donnees sous Formes d'informations
-    getVariableDonnee() {
+    getInformationDonnee() {
         let listeDonnee = this.getDonnee().trim().split(",");
         let listeDonneeInformation = [];
         for(let Donnee of listeDonnee)
@@ -194,7 +199,7 @@ class Probleme extends ElementGraphique {
         return listeDonneeInformation;
     }
     //Retourne les Resultat sous Formes d'informations
-    getVariableResultat() {
+    getInformationResultat() {
         let listeDonnee = this.getResultat().trim().split(",");
         let listeDonneeInformation = [];
         for(let Donnee of listeDonnee)
@@ -210,7 +215,7 @@ class Probleme extends ElementGraphique {
         return listeDonneeInformation;
     }
     // Retourne les variables du textes
-    extraireVariablesTextes() {
+    extraireInformationTextes() {
         let i = new Information();
         i._nom = "";
         const contenue = this.getTexte(); //' aa -> " a a " ';
@@ -231,15 +236,19 @@ class Probleme extends ElementGraphique {
         return i;
     }
     // Retourne les variables 
-    extraireVariables() {
+    extraireInformation() {
         let listeInformation = [];
-        if(this.extraireVariablesTextes())
+        if(this.extraireInformationTextes())
         {
-            listeInformation.push(this.extraireVariablesTextes());
+            listeInformation.push(this.extraireInformationTextes());
         }
-        listeInformation = [...listeInformation,  ...this.getVariableDonnee()]; 
-        listeInformation = [...listeInformation, ...this.getVariableResultat()];
+        listeInformation = [...listeInformation,  ...this.getInformationDonnee()]; 
+        listeInformation = [...listeInformation, ...this.getInformationResultat()];
         return listeInformation;
+    }
+    include(nameInformation)
+    {
+        return this.getTexte().includes(nameInformation) || this.getDonnee().includes(nameInformation) || this.getResultat().includes(nameInformation);
     }
 
 
