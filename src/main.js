4a4cea6fef1ea099a1f3f36da623d3dcee0b7bf4
Fabien Sprocq <sprocq.fabien@gmail.com>
2023-12-19 11:59:51 +0100

    git veut que je rejoute le main

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 document.getElementById('espace1');
 
-let a = new StructureSi("20vw", "20vw", [new Condition("a = 1"),new Condition("a = 2"),new Condition("a = 3"),new Condition("a = 4"),new Condition("a = 5"),new Condition("a = 6"),new Condition("a = 7"),new Condition("a = 8")]);
+let a = new StructureSi("20vw", "20vw", [new Condition("a=1"),new Condition("sinom")]);
 let b = new Probleme;
 let c = new Probleme('0vw','10vw');
 let d = new Probleme('50vw','10vw');
@@ -13,28 +13,13 @@ let i = new Probleme('300vw','10vw');
 document.getElementById('espace1').appendChild(a);
 document.getElementById('espace1').appendChild(b);
 document.getElementById('espace1').appendChild(c);
-document.getElementById('espace1').appendChild(d);
-document.getElementById('espace1').appendChild(e);
-document.getElementById('espace1').appendChild(f);
-document.getElementById('espace1').appendChild(g);
-document.getElementById('espace1').appendChild(h);
-document.getElementById('espace1').appendChild(i);
+
 
 a.afficher();
 b.afficher();
 c.afficher();
-d.afficher();
-e.afficher();
-f.afficher();
-g.afficher();
-h.afficher();
-i.afficher();
+
 
 a._listeConditions.children[0]._elemParent.lierEnfant(b)
 a._listeConditions.children[1]._elemParent.lierEnfant(c);
-a._listeConditions.children[2]._elemParent.lierEnfant(d);
-a._listeConditions.children[3]._elemParent.lierEnfant(e);
-a._listeConditions.children[4]._elemParent.lierEnfant(f);
-a._listeConditions.children[5]._elemParent.lierEnfant(g);
-a._listeConditions.children[6]._elemParent.lierEnfant(h);
-a._listeConditions.children[7]._elemParent.lierEnfant(i);
+
