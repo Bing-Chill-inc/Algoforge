c49386a405f9a0a6e1b8b3e29b80079b540a0c07
Maxime Jacob-Saussereau <maximejacob4000@gmail.com>
2024-01-09 10:01:56 +0100

    ErreurDonneMagique

--- src/PartieErreur/ErreurDonneeMagique.js
+++ src/PartieErreur/ErreurDonneeMagique.js
@@ -25,9 +25,43 @@ class ErreurDonneeMagique extends ErreurConceptuelle
         return "La donnée ", this._nomDonnee," ne provient de nulle part.";
     }
 
-    static detecterAnomalie(unProbleme){
-        let donnees = unProbleme.getDonnee();
-        donnees = donnees.split(',');
-        
+    static detecterAnomalie(unProbleme) {
+        let donnees = unProbleme.getInformationDonnee();
+        if(donnees.length == 0) {
+            return false;
+        }
+        let i = 0;
+        let parent = unProbleme.getParent();
+        if (parent == null) {
+            return false;
+        }
+        let listeEnfantsParent = parent._listeElementsEnfants;
+        while(unProbleme != listeEnfantsParent[i].element) {
+            if(listeEnfantsParent[i] instanceof Probleme || listeEnfantsParent[i] instanceof Procedure){
+                for(let donnee of donnees){
+                    if(listeEnfantsParent[i].getInformationDonnee().includes(donnee)){
+                        return false;
+                    }
+                    else if(listeEnfantsParent[i].getInformationResultat().includes(donnee)){
+                        return false;
+                    }
+                }
+            }
+            i++;
+        }
+        for(let probleme of unProbleme.getAntescedants()) {
+            if(probleme instanceof Probleme || probleme instanceof Procedure) {
+                for(let donnee of donnees){
+                    if(probleme.getInformationDonnee().includes(donnee)) {
+                        return false;   
+                    }
+                    else if(probleme.getInformationResultat().includes(donnee)) {
+                        return false;
+                    }
+                }             
+            }
+            
+        }
+        return true;
     }
 }
