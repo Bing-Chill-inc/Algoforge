873f7f17171024d8125ab98e822bcfb5485daf9f
Samuel <samuelcurran64100@gmail.com>
2024-01-24 10:34:31 +0100

    Ajout des modules express pour node js

--- src/server.js
+++ src/server.js
@@ -1,25 +1,15 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
 const path = require('path');
 
-const server = http.createServer((req, res) => {
-    // Récupérer le chemin du fichier demandé
-    const filePath = path.join(__dirname, 'index.html');
+const app = express();
+const port = 3001;
 
-    // Lire le contenu du fichier
-    fs.readFile(filePath, 'utf8', (err, content) => {
-        if (err) {
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('Internal Server Error');
-        } else {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(content);
-        }
-    });
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-const PORT = 3001;
-
-server.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}/`);
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
 });
