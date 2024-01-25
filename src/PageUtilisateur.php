0c95c5073f331fcecfd3407d5d7600ef442b87b7
Fabien Sprocq <sprocq.fabien@gmail.com>
2024-01-25 11:35:24 +0100

    Barre de tache de la page Utilisateur

---
+++ src/PageUtilisateur.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Page Utilisateur</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <Body>
+        <header id="Barre_Utilisateur">
+            <img src="..\Images\algoforgeLogo.png" alt="Logo Algoforge">
+            <div class="centered">
+                <a><img src="..\Images\NouveauAlgoLogo.PNG" alt="Nouvel Algo"><span>Nouvel Algo</span></a>
+                <a><img src="..\Images\ImporterAlgoLogo.PNG" alt="Importer Algo"><span>Importer Algo</span></a>
+                <a><img src="..\Images\NouveauDossierLogo.PNG" alt="Nouveaux Dossier"><span>Nouveaux Dossier</span></a>
+                <a><img src="..\Images\ImporterDossierLogo.PNG" alt="Importer Dossier"> <span>Importer Dossier</span></a>
+            </div>
+            <div class="right-aligned">
+                <img src="..\Images\RechercheLogo.PNG" alt="Recherche">
+                <img src="..\Images\CompteLogo.PNG" alt="Compte">
+            </div>
+        </header>
+    </Body>
+</html>
+<?php
+    
+?>
