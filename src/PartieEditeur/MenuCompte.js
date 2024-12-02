f7eb142432ae385222053edd928fad9848cf0fb5
Fetnmax <maximejacob4000@gmail.com>
2024-12-02 11:36:24 +0100

    affichage du menu

--- src/PartieEditeur/MenuCompte.js
+++ src/PartieEditeur/MenuCompte.js
@@ -1,64 +1,76 @@
 class MenuCompte extends HTMLElement {
-    // ATTRIBUTS
-    _menuDiv;
-    _boutonDeconnexion;
-    _boutonTheme;
-    _menuIcone;
-    _selectionTheme;
-    _estOuvert = false;
-    _init = false;
+	// ATTRIBUTS
+	_menuDiv;
+	_boutonDeconnexion;
+	_boutonTheme;
+	_menuIcone;
+	_selectionTheme;
+	_estOuvert = false;
 
-    // CONSTRUCTEUR
-    constructor() {
-        super();
+	// CONSTRUCTEUR
+	constructor() {
+		super();
 
-        this.MenuIcone = document.createElement("div");
-        this.MenuIcone.classList.add("img");
-        this.MenuIcone.alt = "Menu compte";
-        this.MenuIcone.id = "MenuCompte";
-        this.MenuIcone.addEventListener("click", () => {
-            if (this._estOuvert) {
-                this.fermerMenu();
-            } else {
-                this.ouvrirMenu();
-            }
-        });
-        this.appendChild(this.MenuIcone);
-    }
+		this.MenuIcone = document.createElement("div");
+		this.MenuIcone.classList.add("img");
+		this.MenuIcone.alt = "Menu compte";
+		this.MenuIcone.id = "MenuCompte";
+		this.MenuIcone.addEventListener("click", () => {
+			if (this._estOuvert) {
+				this.fermerMenu();
+			} else {
+				this.ouvrirMenu();
+			}
+		});
+		this.appendChild(this.MenuIcone);
+	}
 
-    // MÉTHODES
-    ouvrirMenu() {
-        this._menuDiv = document.createElement("div");
-        this._menuDiv.id = "MenuCompteDiv";
-        this._menuDiv.classList.add("menu-compte");
-        this._menuDiv.innerHTML = `
+	// MÉTHODES
+	ouvrirMenu() {
+		this._menuDiv = document.createElement("div");
+		this._menuDiv.id = "MenuCompteDiv";
+		this._menuDiv.classList.add("menu-compte");
+		this._menuDiv.innerHTML = `
             <div class="elementMenuCompte" id="boutonDeconnexion">Deconnexion</div>
+            <div class="elementMenuCompte" id="boutonTheme"></div>
         `;
-        if(!this._init) {
-            this._selectionTheme = document.querySelector("select#theme");
-            this._menuDiv.appendChild(this._selectionTheme);
-            this._selectionTheme.style.display = "block";
-        }
-        
+		let boutonTheme = this._menuDiv.querySelector("#boutonTheme");
+		this._selectionTheme = document.querySelector("select#theme");
+		boutonTheme.appendChild(this._selectionTheme);
 
-        this._boutonDeconnexion = this._menuDiv.querySelector("#boutonDeconnexion");
-        this._boutonDeconnexion.addEventListener("click", () => {
-            this.deconnexion();
-        });
-        this.appendChild(this._menuDiv);
-        this._estOuvert = true;
-        this._init = true;
-    }
+		// style selectionTheme
+		// this._selectionTheme.style.width = "100%";
+		// this._selectionTheme.style.height = "50%";
+		// this._selectionTheme.style.background = "none";
+		// this._selectionTheme.style.border = "var(--fgColor) solid 0.1vw";
+		// this._selectionTheme.style.borderRadius = "1vw";
+		// this._selectionTheme.style.fontSize = "1vw";
+		// this._selectionTheme.style.color = "var(--fgColor)";
+		// this._selectionTheme.style.cursor = "pointer";
+		this._selectionTheme.style.display = "block";
 
-    fermerMenu() {
-        this.removeChild(this._menuDiv);
-        this._estOuvert = false;
-    }
+		this._boutonDeconnexion =
+			this._menuDiv.querySelector("#boutonDeconnexion");
+		this._boutonDeconnexion.addEventListener("click", () => {
+			this.deconnexion();
+		});
+		this.appendChild(this._menuDiv);
+		this._estOuvert = true;
+		this._init = true;
+	}
 
-    deconnexion() {
-        console.log("Déconnexion");
-    }
-    
+	fermerMenu() {
+		this._selectionTheme = document.querySelector("select#theme");
+		this.appendChild(this._selectionTheme);
+		this._selectionTheme.style.display = "none";
+
+		this.removeChild(this._menuDiv);
+		this._estOuvert = false;
+	}
+
+	deconnexion() {
+		console.log("Déconnexion");
+	}
 }
 
 window.customElements.define("menu-compte-element", MenuCompte);
